557c268eec00dd88561862b8a5b74b992859269c
Curtis Rose <ccr46@nau.edu>
2015-03-03 21:14:14 -0700

    New Framework with home page and some css

--- app/urls.py
+++ app/urls.py
@@ -8,10 +8,4 @@ urlpatterns = patterns('',
 
                        url(r'^$', 'app.views.home', name='home'),
 					   url(r'^home/', 'app.views.home', name='home'),
-                       url(r'^campaign/(?P<id>\d+)/', 'app.views.campaign_detail', name="campaign_detail"),
-					   url(r'^thank_you/', 'app.views.thank_you', name="thank_you"),
-                       url(r'^merchandise', 'app.views.merchandise', name='merchandise'),
-                       url(r'^tickets', 'app.views.tickets', name='tickets'),
-                       url(r'^schedule', 'app.views.schedule', name='schedule'),
-                       # url(r'^admin/', include(admin.site.urls)),
 )
--- app/views.py
+++ app/views.py
@@ -4,35 +4,5 @@ from django.http import HttpResponseRedirect
 
 def home(request):
     return render(request, 'home.html', {
-        'campaigns': Campaign.objects.all(),
-        'navoptions': [('About', '#about'), ('Campaigns', '#campaigns'), ('Contact', '#contact')]
-    })
-
-def campaign_detail(request, id):
-    campaign = Campaign.objects.get(pk=int(id))
-    form_class = eval(campaign.formID)
-    if request.method == 'POST':
-        form = form_class(request.POST)
-        if form.is_valid():
-            form.save()
-            name = form.cleaned_data['name']
-            template_params = {
-                'campaign': Campaign.objects.get(pk=int(id)),
-                'form':form,
-                'name':name,
-                'navoptions': [('Go Home', '/home/')]
-            }
-            return render(request, 'thank_you.html', template_params)
-    else:
-        form = form_class
-
-    return render(request, 'campaign_detail.html', {
-        'campaign': Campaign.objects.get(pk=int(id)),
-        'form':form,
-        'navoptions': [('Sign Up', '#sign-up')]
-    })
-
-def thank_you(request):
-    return render(request, 'thank_you.html', {
-        'navoptions': [('Go Home', '/home/')]
+       'navoptions': [('Go Home', '/home/')]
     })
