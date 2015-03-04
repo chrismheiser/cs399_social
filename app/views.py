5982746768aaa8b4892eeb16ef1c2c12d659e782
Curtis Rose <ccr46@nau.edu>
2015-03-03 22:52:05 -0700

    Created a NavBar that can be used on all pages. Doesn't have login option like how activities has it. Didn't change activities nav bar though. Also made all of the other pages that haven't been worked on have a standard layout so people can start working on it and it will work well with base.html now. Just check out home.html if you want to see an example

--- app/views.py
+++ app/views.py
@@ -4,19 +4,41 @@ from django.http import HttpResponseRedirect
 
 def home(request):
     return render(request, 'home.html', {
-       'navoptions': [('Go Home', '/home/')]
+       'navoptions': [('Activities','/activity/'),
+					  ('Login Page','/login/'),
+					  ('About Us','/about/'),
+					  ('Browse','/browse'),
+					  ('Upload','/upload/')]
     })
 
 def login(request):
-    return render(request, 'login.html', {})
+    return render(request, 'login.html', {
+	    'navoptions': [('Go Home', '/home/'),
+	                  ('Activities','/activity/'),
+					  ('About Us','/about/'),
+					  ('Browse','/browse'),
+					  ('Upload','/upload/')]
+		})
 
 
 def about(request):
-    return render(request, 'about.html', {})
+    return render(request, 'about.html', {
+        'navoptions': [('Go Home', '/home/'),
+	                  ('Activities','/activity/'),
+					  ('Login Page','/login/'),
+					  ('Browse','/browse'),
+					  ('Upload','/upload/')]
+    })
 
 
 def browse(request):
-    return render(request, 'browse.html', {})
+    return render(request, 'browse.html', {
+	    'navoptions': [('Go Home', '/home/'),
+	                  ('Activities','/activity/'),
+					  ('Login Page','/login/'),
+					  ('About Us','/about/'),
+					  ('Upload','/upload/')]
+	})
 
 
 def activity(request):
@@ -24,4 +46,10 @@ def activity(request):
 
 
 def upload(request):
-    return render(request, 'upload.html', {})
+    return render(request, 'upload.html', {
+	    'navoptions': [('Go Home', '/home/'),
+	                  ('Activities','/activity/'),
+					  ('Login Page','/login/'),
+					  ('About Us','/about/'),
+					  ('Browse','/browse')]
+		})
