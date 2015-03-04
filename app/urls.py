9ff60d342a045cb205bc019390fb2a443b744c16
cmh553 <cmh553@nau.edu>
2015-03-04 10:15:26 -0700

    Removed bootstrap3 and minor PEP8 stuff.

--- app/urls.py
+++ app/urls.py
@@ -7,10 +7,10 @@ urlpatterns = patterns('',
                        # url(r'^blog/', include('blog.urls')),
 
                        url(r'^$', 'app.views.home', name='home'),
-					   url(r'^home/', 'app.views.home', name='home'),
-					   url(r'^activity/', 'app.views.activity', name='activity'),
-					   url(r'^browse/', 'app.views.browse', name='browse'),
-					   url(r'^about/', 'app.views.about', name='about'),
-					   url(r'^login/', 'app.views.login', name='login'),
-					   url(r'^upload/', 'app.views.upload', name='upload'),
-)
+                       url(r'^home/', 'app.views.home', name='home'),
+                       url(r'^activity/', 'app.views.activity', name='activity'),
+                       url(r'^browse/', 'app.views.browse', name='browse'),
+                       url(r'^about/', 'app.views.about', name='about'),
+                       url(r'^login/', 'app.views.login', name='login'),
+                       url(r'^upload/', 'app.views.upload', name='upload'),
+                       )
